824f01b37a01d64f081d2fd6876ffdc578a3daa1
doraiimond <carl.acevedom@duocuc.cl>
2025-05-29 17:11:27 -0400

    repo prod

---
+++ src/main/java/com/Perfumelandia/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.Perfumelandia.repository;
+
+import com.Perfumelandia.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}

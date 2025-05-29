7ee9d521865558f54737c5770c8f76f788c45c18
doraiimond <robi.contreras@duocuc.cl>
2025-05-29 18:49:52 -0400

    fixv2

---
+++ src/main/resources/static/js/app_reg.js
@@ -0,0 +1,25 @@
+    const API_URL = "http://localhost:8080/api/v1/usuarios";
+    function registrar() {
+      const nombre = document.getElementById("nombre").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      if (!nombre || !email || !password) {
+        alert("por favor completa todos los campos.");
+        return;
+      }
+
+      fetch("http://localhost:8080/api/v1/usuarios", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nombre , email , password })
+      })
+      .then(response => {
+        if (response.ok) {
+          alert("Usuario registrado correctamente");
+          window.location.href = "login.html";
+        } else {
+          alert("Error verifiaca los datos.");
+        }
+      });
+    }

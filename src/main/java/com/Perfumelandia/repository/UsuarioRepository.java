62040e409735e0d4ff9675a52a2488b57dd75afc
doraiimond <carl.acevedom@duocuc.cl>
2025-05-29 17:04:21 -0400

    a

--- src/main/java/com/Perfumelandia/repository/UsuarioRepository.java
+++ src/main/java/com/Perfumelandia/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package com.Perfumelandia.repository;
 
-public class UsuarioRepository {
+import com.Perfumelandia.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmailAndPassword(String email, String password);
 }

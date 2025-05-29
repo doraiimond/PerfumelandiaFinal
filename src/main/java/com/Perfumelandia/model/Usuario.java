48763082581184e01ad3af7fb2ac6093d4264a82
doraiimond <robi.contreras@duocuc.cl>
2025-05-29 18:42:23 -0400

    noti.Html

--- src/main/java/com/Perfumelandia/model/Usuario.java
+++ src/main/java/com/Perfumelandia/model/Usuario.java
@@ -1,5 +1,29 @@
 package com.Perfumelandia.model;
 
-public class Usuario {
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Optional;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String password;
+
+    public static Optional<Usuario> map(Object o){
+    throw new UnsupportedOperationException("Unimplemented method 'map'");
+    }
 }

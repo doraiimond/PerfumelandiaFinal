de8dee0aebfb8f2b16345feb68ec10e06a176880
doraiimond <carl.acevedom@duocuc.cl>
2025-07-10 01:32:53 -0400

    assemblers

--- src/main/java/com/Perfumelandia/assemblers/NotificacionModelAssembler.java
+++ src/main/java/com/Perfumelandia/assemblers/NotificacionModelAssembler.java
@@ -1,5 +1,23 @@
 package com.Perfumelandia.assemblers;
 
-public class NotificacionModelAssembler {
+import com.Perfumelandia.controllerV2.NotificacionControllerV2;
+import com.Perfumelandia.model.Notificacion;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificacionModelAssembler implements RepresentationModelAssembler<Notificacion, EntityModel<Notificacion>> {
+
+    @Override
+    public @NonNull EntityModel<Notificacion> toModel(@NonNull Notificacion notificacion) {
+        return EntityModel.of(notificacion,
+            linkTo(methodOn(NotificacionControllerV2.class).listarNotificaciones()).withRel("notificaciones"),
+            linkTo(methodOn(NotificacionControllerV2.class).crearNotificacion(notificacion)).withRel("crear")
+        );
+    }
 }
--- src/main/java/com/Perfumelandia/assemblers/ProductoModelAssembler.java
+++ src/main/java/com/Perfumelandia/assemblers/ProductoModelAssembler.java
@@ -1,5 +1,24 @@
 package com.Perfumelandia.assemblers;
 
-public class ProductoModelAssembler {
+import com.Perfumelandia.controllerV2.ProductoControllerV2;
+import com.Perfumelandia.model.Producto;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductoModelAssembler implements RepresentationModelAssembler<Producto, EntityModel<Producto>> {
+
+    @Override
+    public @NonNull EntityModel<Producto> toModel(@NonNull Producto producto) {
+        return EntityModel.of(producto,
+            linkTo(methodOn(ProductoControllerV2.class).obtenerProductoPorId(producto.getId())).withSelfRel(),
+            linkTo(methodOn(ProductoControllerV2.class).obtenerProductos()).withRel("productos"),
+            linkTo(methodOn(ProductoControllerV2.class).registrarProducto(producto)).withRel("crear")
+        );
+    }
 }

801b3331ec2ee4223088e282600df45dfcb041ff
amaro392 <fraa.guzman@duocuc.cl>
2025-07-10 01:56:14 -0400

    app_stock.js

--- src/main/resources/static/js/app_stock.js
+++ src/main/resources/static/js/app_stock.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:8080/api/v1/productos")
+  fetch("http://192.168.1.24:8080/api/v2/productos")
     .then(res => res.json())
     .then(data => {
-      data.forEach(producto => {
+      const productos = data._embedded?.productoList || [];
+      productos.forEach(producto => {
         const stockSpan = document.querySelector(`.stock[data-id='${producto.id}']`);
         if (stockSpan) {
           stockSpan.textContent = producto.stock;
